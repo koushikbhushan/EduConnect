2961c57647dded4869c923d33029d4cf855568f9
koushikbhushan <koushikbhushan@gmail.com>
2016-10-23 01:25:52 +0530

    Initial Setup
    Initial setup with Spring dependency

---
+++ src/com/cf/educonnect/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.cf.educonnect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+}
